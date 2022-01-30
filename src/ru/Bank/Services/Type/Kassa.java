b7837c0d3df3dbffe2976062d4e0a983c819ed19
RamilMuratic <ramilmuratov@gmail.com>
2022-01-30 17:14:03 +0500

    Massiv added

--- src/ru/Bank/Services/Type/Kassa.java
+++ src/ru/Bank/Services/Type/Kassa.java
@@ -1,6 +1,13 @@
 package ru.Bank.Services.Type;
 
+import java.lang.reflect.Array;
+
 public class Kassa {
 
-    int [] Array = {1,2,3};
+    public static void main(String[] args) {
+        int [] array = {1,2,3};
+        for( int i: array) {
+            System.out.println(i);
+        }
+    }
 }
